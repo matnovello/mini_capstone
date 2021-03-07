de52625664c9bc023e326a22702e5cc61c27e699
Mat <mvello66@gmail.com>
2021-03-07 11:26:45 -0500

    deletes old data, repopulates with new organized data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new({ name: "raisin bran", price: 3000, image_url: "#", description: "looks gross but tastes alright i guess" })
+product.save
