d793369eb37bc61f731f7e09b8dae32add4304d1
Mat <mvello66@gmail.com>
2021-03-09 21:44:50 -0500

    adds correct migration of Order Q0&%#%

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,2 +1,17 @@
 class Api::OrdersController < ApplicationController
+  def create
+    if current_user
+      order = Order.new(
+        user_id: params[:user_id],
+        product_id: params[:product_id],
+        quantity: params{:quantity],
+        subtotal: product_id.price,
+        total: params[:total],
+        tax: params[:tax],
+
+      )
+    else
+      render json: {message: "No User. Login to submit your order"}
+    end
+  end
 end
