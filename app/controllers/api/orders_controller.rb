81a866698d8a966b7d718f86a1741ac35a0c5536
Mat <mvello66@gmail.com>
2021-03-10 21:35:36 -0500

    working user authentication in show method

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,5 @@
 class Api::OrdersController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user, only: [:destroy, :create, :update]
 
   def create
     if current_user
